b860f625391a0f511916942948afd1cbc5cdc8bc
ib0nk1b0 <iamk1720@gmail.com>
2026-05-30 21:53:20 +0100

    added -n flag to print line numbers; when match is found whole line is printed with the match highlighted red matching linux grep

--- src/wgrep.c
+++ src/wgrep.c
@@ -8,6 +8,14 @@
 #define CUTILS_NO_PREFIX
 #include "cutils.h"
 
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 static char* g_Program;
 
 void usage(FILE* stream, char* program, char* message)
@@ -17,6 +25,7 @@ void usage(FILE* stream, char* program, char* message)
         fprintf(stream, "%s\n", message);
     }
     fprintf(stream, "Usage: %s [OPTIONS] Patterns [FILE]\n", program);
+    fprintf(stream, "       -n    print line number\n");
 }
 
 bool sv_contains(StringView sv, const char* pattern, size_t* out_index)
@@ -37,10 +46,7 @@ bool sv_contains(StringView sv, const char* pattern, size_t* out_index)
 
         for (size_t j = 0; j < pattern_length; j++)
         {
-            char c1 = sv.data[i + j]; 
-            char c2 = pattern[j];
-            // printf("Comparing %c with %c\n", c1, c2);
-            if (c1 != c2)
+            if (sv.data[i + j] != pattern[j])
             {
                 contains = false;
                 break;
@@ -61,10 +67,20 @@ int main(int argc, char** argv)
 
     char* pattern = NULL;
     char* file = NULL;
+    bool print_line_number = false;
     while (argc > 0)
     {
         char* flag = next_cmd_line_arg(&argc, &argv);
-        if (pattern == NULL)
+        if (strcmp(flag, "-n") == 0)
+        {
+            print_line_number = true;
+        }
+        else if (flag[0] == '-')
+        {
+            usage(stderr, g_Program, "Unkown flag provided!");
+            exit(1);
+        }
+        else if (pattern == NULL)
         {
             pattern = flag;
         }
@@ -99,7 +115,14 @@ int main(int argc, char** argv)
         if (contains)
         {
             // printf("("SV_FMT")\n", SV_ARG(line));
-            printf("Found %s in %s at line %zu column %zu\n", pattern, file, line_number, index);
+            StringView lhs = sv_from_parts(line.data, index - 1);
+            size_t eop = index + strlen(pattern) - 1;
+            StringView rhs = sv_from_parts(line.data + eop, line.size - eop);
+            if (print_line_number)
+            {
+                printf(ANSI_COLOR_GREEN"%zu:"ANSI_COLOR_RESET, line_number);
+            }
+            printf(SV_FMT ANSI_COLOR_RED "%s"ANSI_COLOR_RESET SV_FMT"\n", SV_ARG(lhs), pattern, SV_ARG(rhs));
         }
     }
 
