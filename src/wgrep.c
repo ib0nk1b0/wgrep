007519ea9466923c260c17f3806555daddb89c6f
ib0nk1b0 <iamk1720@gmail.com>
2026-06-02 22:02:13 +0100

    added -o flag

--- src/wgrep.c
+++ src/wgrep.c
@@ -26,6 +26,7 @@ void usage(FILE* stream, char* program, char* message)
     }
     fprintf(stream, "Usage: %s [OPTIONS] Patterns [FILE]\n", program);
     fprintf(stream, "       -n    print line number\n");
+    fprintf(stream, "       -o    print only matching part of line\n");
 }
 
 // returns num_matches
@@ -74,12 +75,17 @@ int main(int argc, char** argv)
     char* pattern = NULL;
     char* file = NULL;
     bool print_line_number = false;
+    bool print_only_matching = false;
     while (argc > 0)
     {
         char* flag = next_cmd_line_arg(&argc, &argv);
         if (strcmp(flag, "-n") == 0)
         {
             print_line_number = true;
+        }
+        else if (strcmp(flag, "-o") == 0)
+        {
+            print_only_matching = true;
         }
         else if (flag[0] == '-')
         {
@@ -123,46 +129,28 @@ int main(int argc, char** argv)
                 printf(ANSI_COLOR_GREEN"%zu:"ANSI_COLOR_RESET, line_number);
             }
 
-            // indices[2] = { 0, 21 };
-            // hello, world abcdefg hello, world
-            // ^ next_pattern_start
-            // ^ start_pos
-            // lhs = { data + 0, 0 };
-            // hello, world abcdefg hello, world
-            //                      ^ next_pattern_start
-            //             ^ start_pos
-            // when indices[i] == 0 then start_pos = 0 && next_pattern_start = 0
-            // else if i = 0 then start_pos = 0 && next_pattern_start = indices[i]
-            // else start_pos = indices[i - 1] + pattern_len // maybe + 1
-            // lhs = { data + start_pos, indices[i] - start_pos };
-
-            size_t pattern_len = strlen(pattern) - 1;
-            StringView lhs = sv_from_parts(line.data, indices[0]);
-            printf(SV_FMT, SV_ARG(lhs));
-            printf(ANSI_COLOR_RED "%s"ANSI_COLOR_RESET, pattern);
-            for (size_t i = 1; i < num_matches; i++)
+            if (print_only_matching)
             {
-                size_t start_pos = indices[i - 1] + pattern_len + 1;
-                lhs = sv_from_parts(line.data + start_pos, indices[i] - start_pos);
+                printf(ANSI_COLOR_RED"%s"ANSI_COLOR_RESET"\n", pattern);
+            }
+            else
+            {
+                size_t pattern_len = strlen(pattern) - 1;
+                StringView lhs = sv_from_parts(line.data, indices[0]);
                 printf(SV_FMT, SV_ARG(lhs));
                 printf(ANSI_COLOR_RED "%s"ANSI_COLOR_RESET, pattern);
+                for (size_t i = 1; i < num_matches; i++)
+                {
+                    size_t start_pos = indices[i - 1] + pattern_len + 1;
+                    lhs = sv_from_parts(line.data + start_pos, indices[i] - start_pos);
+                    printf(SV_FMT, SV_ARG(lhs));
+                    printf(ANSI_COLOR_RED "%s"ANSI_COLOR_RESET, pattern);
+                }
+                size_t start_pos = indices[num_matches - 1] + pattern_len + 1;
+                StringView rhs = sv_from_parts(line.data + start_pos, line.size - start_pos);
+                printf(SV_FMT"\n", SV_ARG(rhs));
             }
-            size_t start_pos = indices[num_matches - 1] + pattern_len + 1;
-            StringView rhs = sv_from_parts(line.data + start_pos, line.size - start_pos);
-            printf(SV_FMT"\n", SV_ARG(rhs));
         }
-
-        // if (num_matches)
-        // {
-        //     StringView lhs = sv_from_parts(line.data, index - 1);
-        //     size_t eop = index + strlen(pattern) - 1;
-        //     StringView rhs = sv_from_parts(line.data + eop, line.size - eop);
-        //     if (print_line_number)
-        //     {
-        //         printf(ANSI_COLOR_GREEN"%zu:"ANSI_COLOR_RESET, line_number);
-        //     }
-        //     printf(SV_FMT ANSI_COLOR_RED "%s"ANSI_COLOR_RESET SV_FMT"\n", SV_ARG(lhs), pattern, SV_ARG(rhs));
-        // }
     }
 
     return 0;
